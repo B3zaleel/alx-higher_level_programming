f5b7746647f3bcece748625f1b3296e891ba934a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-07 12:15:29 +0100

    :white_check_mark: Completed task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uppercase(str):
+    '''
+    Prints a string, converting lowercase characters to uppercase
+
+    Parameters:
+    str (str): The string to print
+    '''
+    for i in range(len(str)):
+        is_lower = ((ord(str[i]) >= ord('a')) and (ord(str[i]) <= ord('z')))
+        print('{:s}{:s}'.format(
+            (chr(ord(str[i]) - (1 << 5)) * is_lower),
+            (str[i] * (not is_lower))
+            ), end=('\n' * (i == (len(str) - 1))))
