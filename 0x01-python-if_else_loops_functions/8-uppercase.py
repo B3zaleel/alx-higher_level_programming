16d2fcb05cd2486685e05f49b87bdf72b7843262
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-07 16:07:43 +0100

    :wrench: Adjusted task 8's solution

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -7,9 +7,9 @@ def uppercase(str):
     str (str): The string to print
     '''
     for i in range(len(str)):
-        # is_lower = (ord(str[i]) >= ord('a')) and (ord(str[i]) <= ord('z'))
+        is_lower = (ord(str[i]) >= ord('a')) and (ord(str[i]) <= ord('z'))
         end_str = '\n' * (i == (len(str) - 1))
-        if str[i].islower():
+        if is_lower:
             print('{:c}'.format(ord(str[i]) - (1 << 5)), end=end_str)
         else:
             print('{:c}'.format(ord(str[i])), end=end_str)
