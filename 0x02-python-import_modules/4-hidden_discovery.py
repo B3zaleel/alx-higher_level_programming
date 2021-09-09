3e4e0910091868195fe5cbbfba143ddf3116770b
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-09 18:51:55 +0100

    :wrench: Adjusted algorithm

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == '__main__':
     from importlib import import_module
     hidden_4 = import_module('hidden_4')
     for name in sorted(dir(hidden_4)):
-        if not name.startswith('__'):
+        if name[0:2] != '__':
             print('{:s}'.format(name))
