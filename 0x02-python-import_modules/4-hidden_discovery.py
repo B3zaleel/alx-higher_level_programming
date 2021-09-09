5c89511a54bcca71aea16ceb51f3fa5df349b882
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-09 17:03:13 +0100

    :wrench: Made module item's list sorted

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == '__main__':
     from importlib import import_module
     hidden_4 = import_module('hidden_4')
-    for name in dir(hidden_4):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith('__'):
             print('{:s}'.format(name))
