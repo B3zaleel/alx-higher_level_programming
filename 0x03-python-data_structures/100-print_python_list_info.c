0440d28ffdc88a67f4174dbd54a8d07e336949ed
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-12 20:00:52 +0100

    :wrench: Adjusted algorithm

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <python3.4/object.h>
-#include <python3.4/listobject.h>
+#include <python3.8/object.h>
+#include <python3.8/listobject.h>
 
 /**
  * print_python_list_info - Prints some basic info about a Python list object
@@ -15,5 +15,7 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", list_len);
 	printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
 	for (i = 0; i < list_len; i++)
+	{
 		printf("Element %d: %s\n", i, Py_TYPE(PyList_GetItem(p, i))->tp_name);
+	}
 }
