77afd6a713fd31d1cc4c9eafd69411b4978fb58b
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-12 13:21:13 +0100

    :wrench: Fixed algorithm

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -11,6 +11,6 @@ def replace_in_list(my_list, idx, element):
     Return:
     The modified list
     '''
-    if idx < len(my_list):
+    if idx < len(my_list) and idx >= 0:
         my_list[idx] = element
     return my_list
