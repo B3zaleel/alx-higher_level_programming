29010c1e57dcd7b14d03356ba49153041a56f101
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-15 14:53:48 +0100

    :wrench: Added NULL checks

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -13,18 +13,31 @@
  */
 void print_python_list(PyObject *p)
 {
-	int i;
-	int list_len = (int)(((PyVarObject *)(p))->ob_size);
+	int i, list_len;
     PyObject *item = NULL;
 
     printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %d\n", list_len);
-	printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
-	for (i = 0; i < list_len; i++)
-	{
-        item = (PyObject *)*(((PyListObject *)p)->ob_item + i);
-		printf("Element %d: %s\n", i, (item->ob_type)->tp_name);
-	}
+    if (p != NULL
+        && p->ob_type != NULL
+        && (p->ob_type)->tp_name != NULL
+        && *((p->ob_type)->tp_name + 0) == 'l'
+        && *((p->ob_type)->tp_name + 1) == 'i'
+        && *((p->ob_type)->tp_name + 2) == 's'
+        && *((p->ob_type)->tp_name + 3) == 't')
+    {
+        list_len = (int)(((PyVarObject *)(p))->ob_size);
+        printf("[*] Size of the Python List = %d\n", list_len);
+        printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
+        for (i = 0; i < list_len; i++)
+        {
+            item = (PyObject *)*(((PyListObject *)p)->ob_item + i);
+            printf("Element %d: %s\n", i, (item->ob_type)->tp_name);
+        }
+    }
+    else
+    {
+        printf("  [ERROR] Invalid List Object\n");
+    }
 }
 
 /**
@@ -33,17 +46,20 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-    int i;
-    int bytes_len = (int)(((PyVarObject *)(p))->ob_size);
-    int n = bytes_len < 10 ? bytes_len : 9;
+    int i, bytes_len, n;
 
     printf("[.] bytes object info\n");
-    if (*((p->ob_type)->tp_name + 0) == 'b'
+    if (p != NULL
+        && p->ob_type != NULL
+        && (p->ob_type)->tp_name != NULL
+        && *((p->ob_type)->tp_name + 0) == 'b'
         && *((p->ob_type)->tp_name + 1) == 'y'
         && *((p->ob_type)->tp_name + 2) == 't'
         && *((p->ob_type)->tp_name + 3) == 'e'
         && *((p->ob_type)->tp_name + 4) == 's')
     {
+        bytes_len = (int)(((PyVarObject *)(p))->ob_size);
+        n = bytes_len < 10 ? bytes_len : 9;
         printf("  size: %d\n", bytes_len);
         printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
         printf("  first %d bytes:", n + 1);
