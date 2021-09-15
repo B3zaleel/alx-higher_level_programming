b99daa6e434d056efe78fd4b6b0d559602218315
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-09-15 15:45:07 +0100

    :wrench: Fixed algorithm

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -8,6 +8,44 @@
 #define PY_SSIZE_T_CLEAN
 
 /**
+ * print_python_bytes - Prints some basic info about a Python bytes object
+ * @p: A pointer to the Python bytes object
+ */
+void print_python_bytes(PyObject *p)
+{
+    int i, bytes_len, n;
+
+    printf("[.] bytes object info\n");
+    if (p != NULL
+        && p->ob_type != NULL
+        && (p->ob_type)->tp_name != NULL
+        && *((p->ob_type)->tp_name + 0) == 'b'
+        && *((p->ob_type)->tp_name + 1) == 'y'
+        && *((p->ob_type)->tp_name + 2) == 't'
+        && *((p->ob_type)->tp_name + 3) == 'e'
+        && *((p->ob_type)->tp_name + 4) == 's')
+    {
+        bytes_len = (int)(((PyVarObject *)(p))->ob_size);
+        n = bytes_len < 10 ? bytes_len : 9;
+        printf("  size: %d\n", bytes_len);
+        printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+        if (n > 0)
+        {
+            printf("  first %d bytes:", n + 1);
+            for (i = 0; i < n + 1; i++)
+            {
+                printf(" %02x", (unsigned char)*(((PyBytesObject *)p)->ob_sval + i));
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("  [ERROR] Invalid Bytes Object\n");
+    }
+}
+
+/**
  * print_python_list - Prints some basic info about a Python list object
  * @p: A pointer to the Python list object
  */
@@ -48,43 +86,5 @@ void print_python_list(PyObject *p)
     else
     {
         printf("  [ERROR] Invalid List Object\n");
-    }
-}
-
-/**
- * print_python_bytes - Prints some basic info about a Python bytes object
- * @p: A pointer to the Python bytes object
- */
-void print_python_bytes(PyObject *p)
-{
-    int i, bytes_len, n;
-
-    printf("[.] bytes object info\n");
-    if (p != NULL
-        && p->ob_type != NULL
-        && (p->ob_type)->tp_name != NULL
-        && *((p->ob_type)->tp_name + 0) == 'b'
-        && *((p->ob_type)->tp_name + 1) == 'y'
-        && *((p->ob_type)->tp_name + 2) == 't'
-        && *((p->ob_type)->tp_name + 3) == 'e'
-        && *((p->ob_type)->tp_name + 4) == 's')
-    {
-        bytes_len = (int)(((PyVarObject *)(p))->ob_size);
-        n = bytes_len < 10 ? bytes_len : 9;
-        printf("  size: %d\n", bytes_len);
-        printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
-        if (n > 0)
-        {
-            printf("  first %d bytes:", n + 1);
-            for (i = 0; i < n + 1; i++)
-            {
-                printf(" %02x", (unsigned char)*(((PyBytesObject *)p)->ob_sval + i));
-            }
-            printf("\n");
-        }
-    }
-    else
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
     }
 }
