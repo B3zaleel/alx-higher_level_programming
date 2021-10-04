e2f6d551846e141c9fe49153b5e7aad080235615
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-04 14:54:53 +0100

    :wrench: Adjusted script

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -129,5 +129,7 @@ class Rectangle:
             raise TypeError('rect_1 must be an instance of Rectangle')
         elif not isinstance(rect_2, Rectangle):
             raise TypeError('rect_2 must be an instance of Rectangle')
+        elif rect_1.area() >= rect_2.area():
+            return rect_1
         else:
-            return rect_1 if rect_1.area() >= rect_2.area() else rect_2
+            return rect_2
