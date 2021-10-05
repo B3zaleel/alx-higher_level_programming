1eef4b5afea2505894e77cf615e13579e1171d47
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-05 15:20:42 +0100

    :wrench: Fixed script

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -6,11 +6,4 @@
 class LockedClass:
     '''Represents a class with restricted attribute modification.
     '''
-    def __setattr__(self, name, value):
-        '''Sets an attribute of this class.
-        '''
-        if name == "first_name":
-            self.__dict__[name] = value
-        else:
-            msg = "'LockedClass' object has no attribute '{}'".format(name)
-            raise AttributeError(msg)
+    __slots__ = ['first_name']
