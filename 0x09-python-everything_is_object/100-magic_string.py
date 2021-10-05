997e6bd7a912f567b9c4e8bdb7ba874058e6a7ee
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-05 18:51:35 +0100

    :wrench: Adjusted script

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.c = magic_string.__dict__.get('c', 0) + 1
-    return '{}{}'.format('BestSchool', ', BestSchool' * (magic_string.c - 1))
+    magic_string.c, t = (magic_string.__dict__.get('c', 0) + 1, ', BestSchool')
+    return '{:s}{:s}'.format(t[2:], t * (magic_string.c - 1))
