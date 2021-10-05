ac155277106aaaa5b38bfec165037318950bb44a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-05 11:12:57 +0100

    :white_check_mark: Completed task 30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''A module containing a class with restrictions.
+'''
+
+
+class LockedClass:
+    '''Represents a class with restricted attribute modification.
+    '''
+    def __setattr__(self, name, value):
+        '''Sets an attribute of this class.
+        '''
+        if name == 'first_name':
+            self.__dict__['first_name'] = value
+        else:
+            msg = "'LockedClass' object has no attribute '{}'".format(name)
+            raise AttributeError(msg)
