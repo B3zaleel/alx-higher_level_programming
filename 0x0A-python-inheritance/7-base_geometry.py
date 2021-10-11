2dcea0c150492abc4bc398fd59a2133ed51e1287
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-11 14:41:10 +0100

    Updated algorithm

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -23,6 +23,6 @@ class BaseGeometry:
             value (int): The object to validate.
         '''
         if type(value) is not int:
-            raise TypeError('{:s} must be an integer'.format(name))
+            raise TypeError('{:s} must be an integer'.format(str(name)))
         if value <= 0:
-            raise ValueError('{:s} must be greater than 0'.format(name))
+            raise ValueError('{:s} must be greater than 0'.format(str(name)))
