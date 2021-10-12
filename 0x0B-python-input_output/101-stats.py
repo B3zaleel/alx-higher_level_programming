eb05462713ba733fd5d636304e17d7b1293c8ff7
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-12 19:00:55 +0100

    :wrench: Updated algorithm

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''A script for parsing HTTP logs.
 '''
-# from re import fullmatch
+import re
 
 
 status_codes_stats = {
@@ -51,18 +51,10 @@ def get_metrics(line):
     '''
     global total_file_size
     global status_codes_stats
-    trimmed_line = line.rstrip().lstrip()
-    trimmed_line_parts = trimmed_line.split(' ')
-    # resp_match = fullmatch(log_fmt, trimmed_line)
-    # if resp_match is not None:
-    #     status_code = resp_match.group('status_code')
-    #     file_size = int(resp_match.group('file_size'))
-    #     if status_code in status_codes_stats.keys():
-    #         status_codes_stats[status_code] += 1
-    #         total_file_size += file_size
-    if len(trimmed_line_parts) >= 2:
-        status_code = trimmed_line_parts[-2]
-        file_size = int(trimmed_line_parts[-1])
+    resp_match = re.fullmatch(log_fmt, line.rstrip().lstrip())
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
         if status_code in status_codes_stats.keys():
             status_codes_stats[status_code] += 1
             total_file_size += file_size
@@ -79,7 +71,7 @@ def run():
             line_num += 1
             if line_num % 10 == 0:
                 print_statistics()
-    except (KeyboardInterrupt, EOFError):
+    except Exception:
         print_statistics()
 
 
