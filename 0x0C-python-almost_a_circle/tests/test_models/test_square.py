834e65d15f319b9988a044f8db8d99fd7e7f5c42
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-10-19 02:40:41 +0100

    Updated scripts

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -18,5 +18,53 @@ class TestSquare(unittest.TestCase):
         """
         self.assertIsInstance(Square(5), Base)
         self.assertIsInstance(Square(5), Rectangle)
-        self.assertTrue(issubclass(Square, Rectangle))
         self.assertTrue(issubclass(Square, Base))
+        self.assertTrue(issubclass(Square, Rectangle))
+        polygon = Square(5)
+        id_init = polygon.id
+        self.assertEqual(Square(5, 0, 0, 1).width, 5)
+        self.assertEqual(Square(5, 0, 0, 1).height, 5)
+        self.assertEqual(Square(5, 0, 0, 1).size, 5)
+        self.assertEqual(Square(5, 0, 0, 1).x, 0)
+        self.assertEqual(Square(5, 0, 0, 1).y, 0)
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square('10')
+        self.assertEqual(str(asrt_ctxt.exception), 'width must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(None)
+        self.assertEqual(str(asrt_ctxt.exception), 'width must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(True)
+        self.assertEqual(str(asrt_ctxt.exception), 'width must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, '20')
+        self.assertEqual(str(asrt_ctxt.exception), 'x must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, None)
+        self.assertEqual(str(asrt_ctxt.exception), 'x must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, False)
+        self.assertEqual(str(asrt_ctxt.exception), 'x must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, 20, '25')
+        self.assertEqual(str(asrt_ctxt.exception), 'y must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, 20, None)
+        self.assertEqual(str(asrt_ctxt.exception), 'y must be an integer')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, 20, False)
+        self.assertEqual(str(asrt_ctxt.exception), 'y must be an integer')
+        with self.assertRaises(ValueError) as asrt_ctxt:
+            polygon = Square(0)
+        self.assertEqual(str(asrt_ctxt.exception), 'width must be > 0')
+        with self.assertRaises(ValueError) as asrt_ctxt:
+            polygon = Square(-6)
+        self.assertEqual(str(asrt_ctxt.exception), 'width must be > 0')
+        with self.assertRaises(ValueError) as asrt_ctxt:
+            polygon = Square(6, -3)
+        self.assertEqual(str(asrt_ctxt.exception), 'x must be >= 0')
+        with self.assertRaises(ValueError) as asrt_ctxt:
+            polygon = Square(6, 3, -7)
+        self.assertEqual(str(asrt_ctxt.exception), 'y must be >= 0')
+        with self.assertRaises(TypeError) as asrt_ctxt:
+            polygon = Square(10, 13, 3, 7, 12)
