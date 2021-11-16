321872e0c76ef1e34c7844be054a8b422c0df577
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 06:44:25 +0100

    Fixed Betty style issue

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <python3.4/Python.h>
 #include <python3.4/object.h>
@@ -13,36 +14,30 @@
  */
 void print_python_bytes(PyObject *p)
 {
-    int i, bytes_len, n;
+	int i, bytes_len, n;
 
-    printf("[.] bytes object info\n");
-    if (p != NULL
-        && p->ob_type != NULL
-        && (p->ob_type)->tp_name != NULL
-        && *((p->ob_type)->tp_name + 0) == 'b'
-        && *((p->ob_type)->tp_name + 1) == 'y'
-        && *((p->ob_type)->tp_name + 2) == 't'
-        && *((p->ob_type)->tp_name + 3) == 'e'
-        && *((p->ob_type)->tp_name + 4) == 's')
-    {
-        bytes_len = (int)(((PyVarObject *)(p))->ob_size);
-        n = bytes_len < 10 ? bytes_len : 9;
-        printf("  size: %d\n", bytes_len);
-        printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
-        if (n > 0)
-        {
-            printf("  first %d bytes:", n + 1);
-            for (i = 0; i < n + 1; i++)
-            {
-                printf(" %02x", (unsigned char)*(((PyBytesObject *)p)->ob_sval + i));
-            }
-            printf("\n");
-        }
-    }
-    else
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-    }
+	printf("[.] bytes object info\n");
+	if ((p != NULL) && (p->ob_type != NULL) && ((p->ob_type)->tp_name != NULL)
+		&& (strcmp((p->ob_type)->tp_name, "bytes") == 0))
+	{
+		bytes_len = (int)(((PyVarObject *)(p))->ob_size);
+		n = bytes_len < 10 ? bytes_len : 9;
+		printf("  size: %d\n", bytes_len);
+		printf("  trying string: %s\n", ((PyBytesObject *)p)->ob_sval);
+		if (n > 0)
+		{
+			printf("  first %d bytes:", n + 1);
+			for (i = 0; i < n + 1; i++)
+			{
+				printf(" %02x", (unsigned char)*(((PyBytesObject *)p)->ob_sval + i));
+			}
+			printf("\n");
+		}
+	}
+	else
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+	}
 }
 
 /**
@@ -52,39 +47,29 @@ void print_python_bytes(PyObject *p)
 void print_python_list(PyObject *p)
 {
 	int i, list_len;
-    PyObject *item = NULL;
+	PyObject *item = NULL;
 
-    printf("[*] Python list info\n");
-    if (p != NULL
-        && p->ob_type != NULL
-        && (p->ob_type)->tp_name != NULL
-        && *((p->ob_type)->tp_name + 0) == 'l'
-        && *((p->ob_type)->tp_name + 1) == 'i'
-        && *((p->ob_type)->tp_name + 2) == 's'
-        && *((p->ob_type)->tp_name + 3) == 't')
-    {
-        list_len = (int)(((PyVarObject *)(p))->ob_size);
-        printf("[*] Size of the Python List = %d\n", list_len);
-        printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
-        for (i = 0; i < list_len; i++)
-        {
-            item = (PyObject *)*(((PyListObject *)p)->ob_item + i);
-            printf("Element %d: %s\n", i, (item->ob_type)->tp_name);
-			if (item != NULL
-				&& item->ob_type != NULL
-				&& (item->ob_type)->tp_name != NULL
-				&& *((item->ob_type)->tp_name + 0) == 'b'
-				&& *((item->ob_type)->tp_name + 1) == 'y'
-				&& *((item->ob_type)->tp_name + 2) == 't'
-				&& *((item->ob_type)->tp_name + 3) == 'e'
-				&& *((item->ob_type)->tp_name + 4) == 's')
+	printf("[*] Python list info\n");
+	if ((p != NULL) && (p->ob_type != NULL) && ((p->ob_type)->tp_name != NULL)
+		&& (strcmp((p->ob_type)->tp_name, "list") == 0))
+	{
+		list_len = (int)(((PyVarObject *)(p))->ob_size);
+		printf("[*] Size of the Python List = %d\n", list_len);
+		printf("[*] Allocated = %d\n", (int)((PyListObject *)p)->allocated);
+		for (i = 0; i < list_len; i++)
+		{
+			item = (PyObject *)*(((PyListObject *)p)->ob_item + i);
+			printf("Element %d: %s\n", i, (item->ob_type)->tp_name);
+			if ((item != NULL) && (item->ob_type != NULL)
+				&& ((item->ob_type)->tp_name != NULL)
+				&& (strcmp((item->ob_type)->tp_name, "bytes") == 0))
 			{
 				print_python_bytes(item);
 			}
-        }
-    }
-    else
-    {
-        printf("  [ERROR] Invalid List Object\n");
-    }
+		}
+	}
+	else
+	{
+		printf("  [ERROR] Invalid List Object\n");
+	}
 }
