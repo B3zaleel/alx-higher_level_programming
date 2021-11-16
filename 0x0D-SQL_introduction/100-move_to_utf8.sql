1e2f674354a24cfd8d32783b7e84f8b9cda84232
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 15:20:43 +0100

    :new: Created a new project

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,12 @@
+-- Changes a property of a database in the server
+ALTER DATABASE hbtn_0c_0
+    DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Changes the database in use for the next set of instructions.
+USE hbtn_0c_0;
+-- Changes the properties of a table in the database
+ALTER TABLE first_table
+    DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Changes the properties of a column in a table in the database
+ALTER TABLE first_table
+    MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Inserts a record in a table in the database
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
