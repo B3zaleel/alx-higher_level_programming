a70d4610ea438ea031f5e20978857d7cf07ed95a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 22:33:12 +0100

    Fixed format of keywords

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,6 @@
 ALTER DATABASE hbtn_0c_0
     CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Changes the properties of a table in the database
-Use hbtn_0c_0;
+USE hbtn_0c_0;
 ALTER TABLE first_table
     CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
