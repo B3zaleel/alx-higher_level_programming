468e6c5dc00f5ee379f37cd00efef104c1b1f6ee
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 22:01:35 +0100

    Updated script

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,7 +6,7 @@ ALTER DATABASE hbtn_0c_0
 USE hbtn_0c_0;
 -- Changes the properties of a table in the database
 ALTER TABLE first_table
-    DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Changes the properties of a column in a table in the database
 ALTER TABLE first_table
     MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL;
