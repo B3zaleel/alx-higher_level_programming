e44f516601d8a6783ed30ded1eeb7b6536a532a2
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 22:03:23 +0100

    Updated script

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,9 +1,6 @@
 -- Changes a property of a database in the server
 ALTER DATABASE hbtn_0c_0
     DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Changes the database in use for the next set of instructions.
-USE hbtn_0c_0;
 -- Changes the properties of a table in the database
 ALTER TABLE first_table
     CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
