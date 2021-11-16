cb975ae123d1b5d677c568f27ad434a95e0d11b4
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-11-16 05:44:42 +0100

    Fixed styling issue

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -14,6 +14,8 @@ def only_diff_elements(set_1, set_2):
     new_set_list = []
     new_set_list.extend(set_1)
     new_set_list.extend(set_2)
-    unique = lambda item: not (item in set_1 and item in set_2)
-    new_set_list = filter(unique, new_set_list)
+    new_set_list = filter(
+        lambda item: not (item in set_1 and item in set_2),
+        new_set_list
+    )
     return set(new_set_list)
