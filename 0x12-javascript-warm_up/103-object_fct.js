ba0cf1d65e79e51e8c3c5284918ea83cc25bdbb0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-13 15:38:34 +0100

    Update 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,7 +4,8 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject['incr'] = function () { this.value++; };
+myObject.incr = () => this.value++;
+// myObject.incr = function () { this.value++; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
