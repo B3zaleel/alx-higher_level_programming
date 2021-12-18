2caaa80bf4e9f56cae33646a98e1ccca9624e32e
Bezaleel Olakunori <bezaleeloci@gmail.com>
2021-12-18 12:46:28 +0100

    Update script

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -3,7 +3,7 @@
 Prints all State objects and their City objects in a database.
 """
 import sys
-from sqlalchemy import create_engine, asc
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from relationship_state import Base, State
@@ -15,16 +15,3 @@ if __name__ == '__main__':
         user = sys.argv[1]
         pword = sys.argv[2]
         db_name = sys.argv[3]
-        DATABASE_URL = 'mysql://{}:{}@localhost:3306/{}'.format(
-            user, pword, db_name
-        )
-        engine = create_engine(DATABASE_URL)
-        Base.metadata.create_all(engine)
-        session = sessionmaker(bind=engine)()
-        result = session.query(State).join(City, State.cities).order_by(
-            State.id.asc(), City.id.asc()
-        ).all()
-        for state in result:
-            print('{}: {}'.format(state.id, state.name))
-            for city in state.cities:
-                print('\t{}: {}'.format(city.id, city.name))
