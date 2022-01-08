9e9f56389203405262e0d19c7b04f1bb55ed486a
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 17:05:02 +0100

    :wrench: Remove utf-8 content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
     with request.urlopen(url) as response:
         if response.readable():
-            data = response.read()
+            data = response.read().decode('utf-8')
             print('Body response:')
             print('\t- type: {}'.format(type(data)))
             print('\t- content: {}'.format(data))
