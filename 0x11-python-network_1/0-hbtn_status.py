650615baec8d9dd03269c44607b37c08d98747ae
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 10:00:22 +0100

    :wrench: Change script import style

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Fetches a URL."""
-from urllib import request
+import urllib.request as request
 
 
 if __name__ == '__main__':
-    with request.urlopen('https://intranet.hbtn.io/status') as response:
-        data = response.read()
-        print('Body response:')
-        # tabs = '\t'
-        tabs = '    '
-        print('{}- type: {}'.format(tabs, type(data)))
-        print('{}- content: {}'.format(tabs, data))
-        print('{}- utf8 content: {}'.format(tabs, data.decode('utf-8')))
+    url = 'https://intranet.hbtn.io/status'
+    try:
+        with request.urlopen(url) as response:
+            data = response.read()
+            print('Body response:')
+            print('\t- type: {}'.format(type(data)))
+            print('\t- content: {}'.format(data))
+            print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+    except Exception:
+        pass
