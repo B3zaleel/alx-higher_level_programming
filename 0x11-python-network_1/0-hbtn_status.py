e4dca6122dd1c53b18228ed98f1379e9ffe1b4ab
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 17:02:51 +0100

    :wrench: Change URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ import urllib.request as request
 
 
 if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     with request.urlopen(url) as response:
         if response.readable():
             data = response.read()
