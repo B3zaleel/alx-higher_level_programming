921f25070ef456c4f185d390712379710133195b
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 11:37:18 +0100

    :wrench: Change values printed

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,18 +10,19 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         repository_name = sys.argv[1]
         owner_name = sys.argv[2]
-        req_queries = 'per_page=10'
-        req_url = 'https://api.github.com/repos/{}/{}/commits?{}'.format(
+        api_url = 'https://api.github.com'
+        req_url = '{}/repos/{}/{}/commits?{}'.format(
+            api_url,
             owner_name,
             repository_name,
-            req_queries
+            'per_page=10'
         )
         response = requests.get(
             req_url,
             headers={'Accept': 'application/vnd.github.v3+json'}
         )
         if response.status_code == 200:
-            for commit in map(lambda x: x['commit'], response.json()):
-                commit_sha = commit['tree']['sha']
-                commit_author = commit['committer']['name']
+            for commit in response.json():
+                commit_sha = commit['sha']
+                commit_author = commit['commit']['author']['name']
                 print('{}: {}'.format(commit_sha, commit_author))
