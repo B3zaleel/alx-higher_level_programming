a0966ba77a421df2f8b46ce125557c5c623fb1fa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 06:27:33 +0100

    :wrench: Update POST request format

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,11 +7,10 @@ import requests
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     query = sys.argv[1] if len(sys.argv) > 1 else ""
-    if len(query) > 0 and not query[0].isalpha():
-        query = ""
-    # form_data = [('q', query)]
-    # response = requests.post(url, data=form_data)
-    response = requests.post('{}?q="{}"'.format(url, query))
+    # if len(query) > 0 and not query[0].isalpha():
+    #     query = ""
+    form_data = [('q', query)]
+    response = requests.post(url, data=form_data)
     try:
         json_content = response.json()
         if json_content:
