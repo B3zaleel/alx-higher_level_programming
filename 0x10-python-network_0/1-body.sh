3131cafff857e4b94d7b9576e36a6593450d8994
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 10:27:12 +0100

    :wrench: Change script command

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Gets the body of a response from a URL if the status code is 200
-curl -L "$1"
+curl -s -L "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a DELETE HTTP request to a URL
-curl -X DELETE "$1"
+curl -s -X DELETE "$1"
