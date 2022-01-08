62436f561ef9d97667d524da1dca59b53cd771ec
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 17:00:35 +0100

    :wrench: Add readable condition

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,12 +5,10 @@ import urllib.request as request
 
 if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
-    try:
-        with request.urlopen(url) as response:
+    with request.urlopen(url) as response:
+        if response.readable():
             data = response.read()
             print('Body response:')
             print('\t- type: {}'.format(type(data)))
             print('\t- content: {}'.format(data))
             print('\t- utf8 content: {}'.format(data.decode('utf-8')))
-    except Exception:
-        pass
