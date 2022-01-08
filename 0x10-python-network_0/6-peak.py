2c8afc903b28a3012fb84be36d0fcdfd22e9a3fa
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 16:46:49 +0100

    :wrench: Change peak finding algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,24 +4,21 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
-    if list_of_integers is None or not isinstance(list_of_integers, list):
+    if not isinstance(list_of_integers, list) or len(list_of_integers) == 0:
         return None
-    n = len(list_of_integers)
-    peak = None
-    for i in range(0, n, 1):
-        cur = list_of_integers[i]
-        prev = list_of_integers[i - 1] if i > 0 else None
-        next = list_of_integers[i + 1] if i + 1 < n else None
-        if prev is None:
-            if next is None:
-                peak = cur
+    elif len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        if list_of_integers[1] > list_of_integers[0]:
+            return list_of_integers[1]
         else:
-            if next is None:
-                if cur >= prev:
-                    peak = cur
-            else:
-                if cur > prev and cur > next:
-                    peak = cur
-        if peak is not None:
-            break
-    return peak
+            return list_of_integers[1]
+    else:
+        n = len(list_of_integers)
+        mid = n // 2
+        if list_of_integers[mid] < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[0:mid - 1])
+        elif list_of_integers[mid] < list_of_integers[mid + 1]:
+            return find_peak(list_of_integers[mid + 1:n])
+        else:
+            return list_of_integers[mid]
