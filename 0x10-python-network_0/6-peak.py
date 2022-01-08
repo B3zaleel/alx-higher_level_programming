026827f20224c992be98784cf7e892d24a45dff0
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 20:48:56 +0100

    :wrench: Change peak finding algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,19 +6,12 @@ def find_peak(list_of_integers):
     """Finds a peak in a list of unsorted integers."""
     if not isinstance(list_of_integers, list) or len(list_of_integers) == 0:
         return None
-    elif len(list_of_integers) == 1:
-        return list_of_integers[0]
-    elif len(list_of_integers) == 2:
-        if list_of_integers[1] > list_of_integers[0]:
-            return list_of_integers[1]
-        else:
-            return list_of_integers[1]
     else:
         n = len(list_of_integers)
         mid = n // 2
-        if list_of_integers[mid] < list_of_integers[mid - 1]:
-            return find_peak(list_of_integers[0:mid - 1])
-        elif list_of_integers[mid] < list_of_integers[mid + 1]:
-            return find_peak(list_of_integers[mid + 1:n])
+        if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            return find_peak(list_of_integers[0 : mid - 1])
+        elif mid + 1 < n and list_of_integers[mid] < list_of_integers[mid + 1]:
+            return find_peak(list_of_integers[mid : n])
         else:
             return list_of_integers[mid]
