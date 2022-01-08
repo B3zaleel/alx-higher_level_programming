c9c84fc42d22cc624a8ec1513300200f65070965
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-08 17:04:09 +0100

    :wrench: Remove utf-8 content

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,11 @@ import urllib.request as request
 
 
 if __name__ == '__main__':
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = 'https://intranet.hbtn.io/status'
     with request.urlopen(url) as response:
         if response.readable():
             data = response.read()
             print('Body response:')
             print('\t- type: {}'.format(type(data)))
             print('\t- content: {}'.format(data))
-            print('\t- utf8 content: {}'.format(data.decode('utf-8')))
+            # print('\t- utf8 content: {}'.format(data.decode('utf-8')))
