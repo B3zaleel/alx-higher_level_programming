437c2aa11600b2b13fe9337d30418d3f241151d1
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-09 17:06:04 +0100

    :wrench: Change text output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,17 +4,10 @@ import urllib.request as request
 
 
 if __name__ == "__main__":
-    try:
-        with request.urlopen("https://intranet.hbtn.io/status") as response:
-            if response.readable():
-                data = response.read()
-                raise ValueError('')
-                print("Body response:")
-                print("\t- type: {}".format(type(data)))
-                print("\t- content: {}".format(data))
-                print("\t- utf8 content: {}".format(data.decode("utf-8")))
-    except Exception:
-        print("Body response:")
-        print("\t- type: <class 'bytes'>")
-        print("\t- content: b'OK'")
-        print("\t- utf8 content: OK")
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        if response.readable():
+            data = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(data)))
+            print("\t- content: {}".format(data))
+            print("\t- utf8 content: {}".format(data.decode("utf-8")))
