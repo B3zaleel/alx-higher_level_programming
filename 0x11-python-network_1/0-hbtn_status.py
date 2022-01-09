91d2092c2311f1c1a82eeb94daf3e4656019857c
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-01-09 14:34:28 +0100

    :wrench: Change text output

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     with request.urlopen("https://intranet.hbtn.io/status") as response:
         if response.readable():
             data = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(data)))
-            print("\t- content: {}".format(data))
-            print("\t- utf8 content: {}".format(data.decode("utf-8")))
+            print("Body response:\n")
+            print("\t- type: {}\n".format(type(data)))
+            print("\t- content: {}\n".format(data))
+            print("\t- utf8 content: {}\n".format(data.decode("utf-8")))
