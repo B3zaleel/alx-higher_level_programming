a3a23a1473aabf68fc0a3e4a604e749949437006
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-02-01 11:24:07 +0100

    Change filtering method

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,13 @@
 const request = require('request');
 
 if (process.argv.length > 2) {
-  const charId = 'https://swapi-api.hbtn.io/api/people/18/';
-
   request(`${process.argv[2]}`, (err, res, body) => {
     if (err) {
       console.log(err);
     } else if (body) {
-      const films = JSON.parse(body).results;
-      const charFilms = films.filter(x => x.characters.includes(charId));
+      const charFilms = JSON.parse(body).results.filter(
+        x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+      );
 
       console.log(charFilms.length);
     }
